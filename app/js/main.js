9d996f7b1e3553c766035110402308f22cd38493
Dkorwin <korwin06987@gmail.com>
2021-11-16 13:44:10 +0300

    product+slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: '12px',
+        readOnly: true
+      });
 
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 4
+    });
 
-  
+    var mixer = mixitup('.products__inner-box');
+
+    
 });
